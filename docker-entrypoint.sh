128011938682a759f8efa0d57fe1849ba7ec47fe
Merlin Rabens <m.rabens@hotmail.de>
2017-09-22 15:23:55 +0200

    Update docker-entrypoint.sh
    Made IOTA ports configurable

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java -XX:+DisableAttachMechanism -Xmx$MEMORY -Xms$MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
+p="${API_PORT:-14265}"
+u="${UDP_PORT:-14600}"
+t="${TCP_PORT:-15600}"
 
+exec java -XX:+DisableAttachMechanism -Xmx$MEMORY -Xms$MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p $p -u $u -t $t -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
