be59c23e36ff06103c50ab97bfea0ebf136346cb
root <root@node1.iota.buaho.net>
2017-09-26 18:11:05 +0000

    Removed ipv4 Preference from iri start, Removed limitation of remote api, addee docker login before push

--- build.sh
+++ build.sh
@@ -1,4 +1,9 @@
-cd /home/ec2-user/docker/
-docker build iota-node --no-cache --tag iota-node:latest
+cd /root
+docker build iota-node --tag iota-node:latest
+#docker build iota-node --no-cache --tag iota-node:latest
+
+read -p "Username:" username
+read -p "Password:" password
+docker login --username $username --password $password
 docker tag iota-node kaidehling/iota-node:latest
 docker push kaidehling/iota-node:latest
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,6 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java -XX:+DisableAttachMechanism -Xmx$MEMORY -Xms$MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
+#exec java -XX:+DisableAttachMechanism -Xmx$MEMORY -Xms$MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" --send-limit 100.0 "$@"
+exec java -XX:+DisableAttachMechanism -Xmx$MEMORY -Xms$MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --max-peers 0 "$@"
 
