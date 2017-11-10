c195fe6dd439786ac976d2d0ac2fced0d1a9da04
kai.dehling <kai.dehling@gmail.com>
2017-11-10 06:46:14 +0000

    Added --add-modules as a workaround for javax.xml.bind classes missing in jdk9. Added parameter to pass arguments to run.sh script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,5 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+DisableAttachMechanism -Xms$MIN_MEMORY -Xmx$MAX_MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" "$@"
+exec java --add-modules java.xml.bind -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+DisableAttachMechanism -Xms$MIN_MEMORY -Xmx$MAX_MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" "$@"
 
