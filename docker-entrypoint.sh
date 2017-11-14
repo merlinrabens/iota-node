b92882f745d9f526a49ec976b13586f553830ac6
Merlin Rabens <m.rabens@hotmail.de>
2017-11-14 15:33:49 +0100

    Update docker-entrypoint.sh
    Set ports from outside

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,5 +6,8 @@ for buddy in $(cat /iri/conf/neighbors); do
 done
 neighbors=${neighbors::-1}
 
-exec java --add-modules java.xml.bind -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+DisableAttachMechanism -Xms$MIN_MEMORY -Xmx$MAX_MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p 14265 -u 14777 -t 15777 -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" "$@"
+p="${API_PORT:-14265}"
+u="${UDP_PORT:-14600}"
+t="${TCP_PORT:-15600}"
 
+exec java --add-modules java.xml.bind -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:+DisableAttachMechanism -Xms$MIN_MEMORY -Xmx$MAX_MEMORY -Dlogback.configurationFile=/iri/conf/logback.xml -Djava.net.preferIPv4Stack=true -jar /iri/target/iri*.jar -p $p -u $u -t $t -n "$neighbors" --remote --remote-limit-api "attachToTangle, addNeighbors, removeNeighbors" "$@"
